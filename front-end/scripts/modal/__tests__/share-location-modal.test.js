dfc0169ab483f15930f4d3f2af83b25cf3343e82
lana.gordon <lana.gordon@slalom.com>
2023-10-20 13:31:49 +0100

    Modal tests

--- front-end/scripts/modal/__tests__/share-location-modal.test.js
+++ front-end/scripts/modal/__tests__/share-location-modal.test.js
@@ -1,5 +1,25 @@
+import { JSDOM } from 'jsdom';
+import {
+	showModal,
+	allowSharing,
+	denySharing,
+	closeModal,
+} from '../share-location-modal';
+
+//  Setup
+const htmlMock = `
+<div id="modal">
+<button id="modal_content__buttons--cancel">Cancel</button>
+<button id="modal_content__buttons--allow">Allow</button>
+</div>`;
+
+const dom = new JSDOM(htmlMock);
+global.document = dom.window.document;
+
 describe('Share Location Pop Up Modal', () => {
-	test('test', () => {
-		console.log('testing');
+	it('shows modal on share button click ', () => {
+		showModal();
+
+		exp;
 	});
 });
